d978d6897024a7e2ba96c77f886ffcf3675b38be
SYLLA David <d.sylla@farms-digital.com>
2026-04-17 14:50:38 +0200

    logofarms

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
 })
-export class Header {}
+export class Header {
+  isOpen = false;
+  selectedLang = 'FR';
+
+  flags: { [key: string]: string } = {
+    FR: '../../../assets/images/france.png',
+    EN: '../../../assets/images/royaume-uni.png',
+    ES: '../../../assets/images/drapeau.png',
+    PT: '../../../assets/images/bresil.png',
+  };
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectLang(lang: string) {
+    this.selectedLang = lang;
+    this.isOpen = false;
+  }
+}
