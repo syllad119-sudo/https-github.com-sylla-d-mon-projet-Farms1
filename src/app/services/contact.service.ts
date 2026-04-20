9d9f9a015c7d947dbbcbb6c37746623e82fddf1a
SYLLA David <d.sylla@farms-digital.com>
2026-04-20 15:33:54 +0200

    lodash

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Contact } from '../models/contact.model';
 import { ContactForm } from '../models/contactForm.model';
 import CONTACTS from '../../assets/contact.json';
+import _ from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  private contacts: Contact[] = [...CONTACTS];
+  private contacts: Contact[] = _.cloneDeep(CONTACTS) as Contact[];
 
   getContacts(): Contact[] {
     return this.contacts;
@@ -15,27 +16,27 @@ export class ContactService {
 
   addContact(form: ContactForm): Contact[] {
     const newContact: Contact = {
-      id: this.contacts.length + 1,
-      societe: form.societe,
-      nom: form.nom,
-      prenom: form.prenom,
-      pays: form.pays,
+      id: _.maxBy(this.contacts, 'id')?.id ?? 0 + 1, // ← id unique basé sur le max
+      ..._.pick(form, ['societe', 'nom', 'prenom', 'pays']), // ← pick les champs utiles
     };
     this.contacts = [newContact, ...this.contacts];
     return this.contacts;
   }
 
   updateContact(id: number, form: ContactForm): Contact[] {
-    this.contacts = this.contacts.map(c =>
-      c.id === id
-        ? { ...c, societe: form.societe, nom: form.nom, prenom: form.prenom, pays: form.pays }
-        : c
-    );
+    const index = _.findIndex(this.contacts, { id }); // ← trouve l'index par id
+    if (index !== -1) {
+      this.contacts[index] = {
+        ...this.contacts[index],
+        ..._.pick(form, ['societe', 'nom', 'prenom', 'pays']), // ← merge uniquement les champs utiles
+      };
+      this.contacts = _.cloneDeep(this.contacts); // ← force la détection de changement Angular
+    }
     return this.contacts;
   }
 
   removeContact(index: number): Contact[] {
-    this.contacts = this.contacts.filter((_, i) => i !== index);
+    this.contacts = _.filter(this.contacts, (_, i) => i !== index); // ← filter lodash
     return this.contacts;
   }
 }
